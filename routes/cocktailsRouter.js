ee6a9f8e3f91e1750dba7d6626c98a0a17920640
Derek <derek.chan522@gmail.com>
2018-07-24 11:18:13 +0800

    seperate cocktailsRouter into public and restricted routes

--- routes/cocktailsRouter.js
+++ routes/cocktailsRouter.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const Cocktail = require("../models/cocktail");
-const router = express.Router();
+const publicRoute = express.Router();
+const restrictedRoute = express.Router();
 const authenticateUser = require("../utils/auth");
 
-router.use(express.json());
+publicRoute.use(express.json());
 
-router.get("/", async (req, res, next) => {
+publicRoute.get("/", async (req, res, next) => {
   const cocktails = await Cocktail.find();
   res.json(cocktails);
 });
 
-router.get("/search", async (req, res) => {
+publicRoute.get("/search", async (req, res) => {
   var cocktails = await Cocktail.find({ name: req.query.name });
   res.json(cocktails);
 });
 
-router.post("/", authenticateUser, async (req, res) => {
+publicRoute.use(function(req, res, next) {
+  res.status(404).send({ Message: "Cocktail Unavailable!" });
+});
+
+restrictedRoute.post("/", async (req, res) => {
   const cocktail = await new Cocktail();
   cocktail.name = req.body.name;
   cocktail.save(function(err) {
@@ -24,20 +29,17 @@ router.post("/", authenticateUser, async (req, res) => {
   });
 });
 
-router.put("/:id", authenticateUser, async (req, res, next) => {
+restrictedRoute.put("/:id", async (req, res, next) => {
   const update = await Cocktail.findByIdAndUpdate(req.params.id, req.body);
   res.status(204).json();
 });
 
-router.delete("/:id", authenticateUser, async (req, res, next) => {
+restrictedRoute.delete("/:id", async (req, res, next) => {
   const update = await Cocktail.findByIdAndDelete(req.params.id, req.body);
   res.status(204).json();
 });
 
-router.use(function(req, res, next) {
-  res.status(404).send({ Message: "Cocktail Unavailable!" });
-});
-
 module.exports = app => {
-  app.use("/cocktails", router);
+  app.use("/cocktails", authenticateUser, restrictedRoute);
+  app.use("/cocktails", publicRoute);
 };
